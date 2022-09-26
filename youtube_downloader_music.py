911260ecf5a62acb2e6b461745c98b9283a6ebdf
JXLK-dev <minemaster151@gmail.com>
2022-09-26 10:26:34 +0700

    form code from the ipynb to py, download prevents

---
+++ youtube_downloader_music.py
@@ -0,0 +1,45 @@
+import os
+import yt_dlp
+import fileinput
+import os.path
+
+
+def run(video_url: str):
+    video_info = yt_dlp.YoutubeDL().extract_info(
+        url=video_url, download=False
+    )
+    filename = f"{video_info['title']}.mp3"
+    # to check if music existed
+    file_exists = os.path.exists(filename)
+    # print(file_exists)
+
+    options = {
+        'format': 'bestaudio/best',
+        'keepvideo': False,
+        'outtmpl': filename,
+    }
+    if (not file_exists):
+        with yt_dlp.YoutubeDL(options) as ydl:
+            ydl.download([video_info['webpage_url']])
+
+        print("Download complete... {}".format(filename))
+    else:
+        print("Failed to download file\n filename : {} has existed".format(filename))
+
+
+def extract_text_file():
+    file = fileinput.input('list_of_songs.txt')
+    listofsongs = []
+    for line in file:
+        listofsongs.append(line)
+    return listofsongs
+
+
+def download_all_script():
+    listofsongs = extract_text_file()
+    for song in listofsongs:
+        run(song)
+
+
+if __name__ == '__main__':
+    download_all_script()
